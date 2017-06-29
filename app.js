381f5e850975130589af3a330e34e79609a2edeb
Craig Oda <craig@o3rocket.com>
2017-06-28 21:24:08 -0700

    added take picture feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var electron = require('electron');
 
 electron.app.on('ready', function () {
-    var mainWindow = new electron.BrowserWindow({width: 800, height: 600});
+    var mainWindow = new electron.BrowserWindow({width: 800, height: 800});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 require('node-rest-client');
 
-var thetaResponse = document.getElementById('thetaResponse');
-thetaResponse.innerHTML = ("this is a test response");
-
 var Client = require('node-rest-client').Client;
 
 var client = new Client();
@@ -12,6 +9,61 @@ getInfo = function() {
     client.get("http://192.168.1.1:80/osc/info", function (data, response) {
       console.log(data);
       // console.log(response);
+    var thetaResponse = document.getElementById('thetaResponse');
+    thetaResponse.innerHTML = JSON.stringify(data);
+    });
+  }
+
+state = function() {
+    var args = {
+    data: { },
+    headers: { "Content-Type": "application/json" }
+};
+    client.post("http://192.168.1.1:80/osc/state", args, function (data, response) {
+      console.log(data);
+    var thetaResponse = document.getElementById('thetaResponse');
+    thetaResponse.innerHTML = JSON.stringify(data);
+    });
+  }
+
+startSession = function() {
+    var args = {
+    data: { "name": "camera.startSession" },
+    headers: { "Content-Type": "application/json" }
+};
+    client.post("http://192.168.1.1:80/osc/commands/execute", args, function (data, response) {
+      console.log(data);
+    var thetaResponse = document.getElementById('thetaResponse');
+    thetaResponse.innerHTML = JSON.stringify(data);
+    });
+  }
+
+takePicture = function() {
+    var args = {
+    data: { "name": "camera.takePicture"},
+    headers: { "Content-Type": "application/json" }
+};
+    client.post("http://192.168.1.1:80/osc/commands/execute", args, function (data, response) {
+      console.log(data);
+    var thetaResponse = document.getElementById('thetaResponse');
+    thetaResponse.innerHTML = JSON.stringify(data);
+    });
+  }
+
+setApiV2 = function() {
+    var args = {
+    data: { "name": "camera.setOptions",
+          "parameters":{
+            "sessionId": "SID_0001",
+            "options": {
+              "clientVersion": 2
+            }
+          } 
+        },
+    headers: { "Content-Type": "application/json" }
+};
+    client.post("http://192.168.1.1:80/osc/commands/execute", args, function (data, response) {
+      console.log(data);
     var thetaResponse = document.getElementById('thetaResponse');
     thetaResponse.innerHTML = JSON.stringify(data);
     });
